bb10d7b5e5719861009cbe264709d592b4074edb
Riam Martinelli <107009834+richboyyy@users.noreply.github.com>
2025-07-10 10:34:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ st.markdown("""
 # ===== Título e logo =====
 col1, col2 = st.columns([1, 5])
 with col1:
-    st.image("logo_ouvidoria_anvisa.png", width=140)
+    st.image("logo_ouvidoria_anvisa.png", width=1000)
 with col2:
     st.markdown("<h1 style='margin-top: 40px;'>Hub de Serviços da Ouvidoria - ANVISA</h1>", unsafe_allow_html=True)
     st.markdown("Acesse rapidamente os principais canais de participação e atendimento:")
@@ -53,25 +53,25 @@ servicos = [
     {
         "titulo": "Registrar Manifestação (Fala.BR)",
         "descricao": "Envie sugestões, denúncias, reclamações ou elogios por meio da plataforma Fala.BR.",
-        "imagem": "falabr.png",
+        "imagem": "images/falabr.png",
         "link": "https://falabr.cgu.gov.br/"
     },
     {
         "titulo": "Carta de Serviços da Anvisa",
         "descricao": "Conheça os serviços prestados pela Anvisa e seus compromissos de atendimento.",
-        "imagem": "Anvisa-Logo-1.png",
+        "imagem": "images/Anvisa-Logo-1.png",
         "link": "https://www.gov.br/anvisa/pt-br/carta-de-servicos"
     },
     {
         "titulo": "Ouvidoria Geral do SUS",
         "descricao": "Acesse o canal nacional de ouvidoria do Sistema Único de Saúde.",
-        "imagem": "ouvidoriasus.png",
+        "imagem": "images/ouvidoriasus.png",
         "link": "https://www.gov.br/saude/pt-br/assuntos/saude-ouvidoria"
     },
     {
         "titulo": "Contato com a Ouvidoria da Anvisa",
         "descricao": "Fale diretamente com a ouvidoria institucional da Anvisa.",
-        "imagem": "logo_ouvidoria_anvisa.png",
+        "imagem": "images/logo_ouvidoria_anvisa.png",
         "link": "https://www.gov.br/anvisa/pt-br/canais_atendimento/ouvidoria"
     },
 ]
